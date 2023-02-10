2ff40d9f85d081db47683bbfa071cb9e2f1e466f
arkaitzna <arkaitzara@gmail.com>
2023-02-10 09:32:07 +0100

    new

---
+++ Banco/JS/main.js
@@ -0,0 +1,4 @@
+console.log('Hello world')
+var alert1=5;
+let alert1;
+print(alert1)
---
+++ JS/main.js
@@ -0,0 +1,26 @@
+let num1=1;
+let num2=2;
+let num3=3;
+let suma=num1+num2+num3;
+console.log(suma);
+num3=8;
+suma=num2+num3
+console.log(suma);
+
+let a,b,c;
+a=1;
+b=2;
+c=3;
+console.log(a+b+c)
+
+let t="5"
+const r=parseInt(t)
+console.log(r)
+
+const w=5
+console.log(String(w))
+
+const x=true
+console.log(String(x))
+ const y=true
+ console.log(y)
