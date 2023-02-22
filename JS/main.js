60092291d4b9e1d69463604f0e93e0ff3e4b94ab
arkaitzna <arkaitzara@gmail.com>
2023-02-22 12:09:28 +0100

    kfg

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,36 @@
-let num1=1;
-let num2=2;
-let num3=3;
-let suma=num1+num2+num3;
-console.log(suma);
-num3=8;
-suma=num2+num3
-console.log(suma);
+// let num1=1;
+// let num2=2;
+// let num3=3;
+// let suma=num1+num2+num3;
+// console.log(suma);
+// num3=8;
+// suma=num2+num3
+// console.log(suma);
 
-let a,b,c;
-a=1;
-b=2;
-c=3;
-console.log(a+b+c)
+// let a,b,c;
+// a=1;
+// b=2;
+// c=3;
+// console.log(a+b+c)
 
-let t="5"
-const r=parseInt(t)
-console.log(r)
+// let t="5"
+// const r=parseInt(t)
+// console.log(r)
 
-const w=5
-console.log(String(w))
+// const w=5
+// console.log(String(w))
 
-const x=true
-console.log(String(x))
- const y=true
- console.log(y)
+// const x=true
+// console.log(String(x))
+//  const y=true
+//  console.log(y)
+
+
+const coches = ['BMW', 'Volvo', 'Volkswagen'];
+const lon= coches.length;
+// for(let i= 0; i < lon; i++){
+//     console.log(coches[i]);
+// }
+for(let coche of coches){
+    console.log(coche)
+}
---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Directive1Directive } from './directives/directive1.directive';
+import { Pipe2Pipe } from './pipes/pipe2.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component2Component,
+    Directive1Directive,
+    Pipe2Pipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
