f601278ff54197909608c0311a3e093a6875e012
arkaitzna <arkaitzara@gmail.com>
2023-03-14 11:56:02 +0100

    iuhgwef
    wouih

--- eclipse/PrimerosPasos/src/String2.java
+++
@@ -1,9 +0,0 @@
-public class String2 {
-
-	public static void main(String[] args) {
-		String frase="Hoy es un estupendo dia para aprender a programar Java";
-		
-		String fraseResumen=frase.substring(0, 29) + "para ir a clase y " + frase.substring(29);
-		System.out.println(fraseResumen);
-	}
-}
